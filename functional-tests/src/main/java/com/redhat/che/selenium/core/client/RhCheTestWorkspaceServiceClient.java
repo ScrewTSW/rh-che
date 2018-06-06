c23df39ffdd2f518144056750f116fff784996ac
Tibor Dancs <tdancs@redhat.com>
2018-06-06 09:20:20 +0200

    Reworked TestWorkspaceServiceClient to Abstract + RhChe implementation

--- functional-tests/src/main/java/com/redhat/che/selenium/core/client/RhCheTestWorkspaceServiceClient.java
+++ functional-tests/src/main/java/com/redhat/che/selenium/core/client/RhCheTestWorkspaceServiceClient.java
@@ -1,26 +1,21 @@
 package com.redhat.che.selenium.core.client;
 
 import com.redhat.che.selenium.core.workspace.CheStarterWrapper;
-import java.io.IOException;
-import org.eclipse.che.api.core.BadRequestException;
-import org.eclipse.che.api.core.ConflictException;
-import org.eclipse.che.api.core.ForbiddenException;
-import org.eclipse.che.api.core.NotFoundException;
-import org.eclipse.che.api.core.ServerException;
-import org.eclipse.che.api.core.UnauthorizedException;
 import org.eclipse.che.api.core.model.workspace.Workspace;
 import org.eclipse.che.api.core.model.workspace.WorkspaceStatus;
 import org.eclipse.che.api.core.rest.HttpJsonRequestFactory;
+import org.eclipse.che.api.workspace.shared.dto.WorkspaceConfigDto;
 import org.eclipse.che.api.workspace.shared.dto.WorkspaceDto;
-import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClient;
+import org.eclipse.che.selenium.core.client.AbstractTestWorkspaceServiceClient;
 import org.eclipse.che.selenium.core.provider.TestApiEndpointUrlProvider;
 import org.eclipse.che.selenium.core.requestfactory.TestUserHttpJsonRequestFactoryCreator;
 import org.eclipse.che.selenium.core.user.DefaultTestUser;
 import org.eclipse.che.selenium.core.user.TestUser;
+import org.eclipse.che.selenium.core.workspace.MemoryMeasure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class RhCheTestWorkspaceServiceClient extends TestWorkspaceServiceClient {
+public class RhCheTestWorkspaceServiceClient extends AbstractTestWorkspaceServiceClient {
 
   private static final Logger LOG = LoggerFactory.getLogger(RhCheTestWorkspaceServiceClient.class);
   private static final CheStarterWrapper cheStarterWrapper = CheStarterWrapper.getInstance();
@@ -38,14 +33,19 @@ public class RhCheTestWorkspaceServiceClient extends TestWorkspaceServiceClient 
     super(apiEndpointProvider, userHttpJsonRequestFactoryCreator, testUser);
   }
 
-  public Workspace createWorkspaceWithCheStarter(String username)
-      throws ForbiddenException, BadRequestException, IOException, ConflictException, NotFoundException, ServerException, UnauthorizedException {
-    String name = cheStarterWrapper.createWorkspace("create-workspace-request.json");
-    return requestFactory.fromUrl(getNameBasedUrl(name, username)).request()
+  //TODO: FIX
+  @Override
+  public Workspace createWorkspace(String workspaceName, int memory, MemoryMeasure memoryUnit,
+      WorkspaceConfigDto workspace) throws Exception {
+    String wkspName = cheStarterWrapper.createWorkspace("create-workspace-request.json");
+    return requestFactory.fromUrl(getNameBasedUrl(wkspName, username)).request()
         .asDto(WorkspaceDto.class);
   }
 
-  public void startWithCheStarter(Workspace ws, String name, DefaultTestUser owner) {
+  //TODO: FIX
+  @Override
+  public void start(String workspaceId, String workspaceName, DefaultTestUser workspaceOwner)
+      throws Exception {
     try {
       cheStarterWrapper.startWorkspace(ws, name);
       waitStatus(name, owner.getName(), WorkspaceStatus.RUNNING);
