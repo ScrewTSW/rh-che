9e0816eabf27b21132fe6290d9c850caebbd51fa
Tibor Dancs <tdancs@redhat.com>
2018-07-03 15:16:35 +0200

    Fixed RhCheSeleniumSuiteModule issue with factory install

--- functional-tests/src/main/java/com/redhat/che/selenium/core/RhCheSeleniumSuiteModule.java
+++ functional-tests/src/main/java/com/redhat/che/selenium/core/RhCheSeleniumSuiteModule.java
@@ -3,12 +3,9 @@ package com.redhat.che.selenium.core;
 import static com.google.inject.name.Names.named;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Inject;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
-import com.google.inject.name.Named;
 import com.redhat.che.selenium.core.client.RhCheTestWorkspaceServiceClient;
 import com.redhat.che.selenium.core.workspace.RhCheTestWorkspaceProvider;
-import org.eclipse.che.selenium.core.client.AbstractTestWorkspaceServiceClient;
 import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClient;
 import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClientFactory;
 import org.eclipse.che.selenium.core.configuration.SeleniumTestConfiguration;
@@ -17,18 +14,8 @@ import org.eclipse.che.selenium.core.provider.DefaultTestUserProvider;
 import org.eclipse.che.selenium.core.user.DefaultTestUser;
 import org.eclipse.che.selenium.core.user.MultiUserCheDefaultTestUserProvider;
 import org.eclipse.che.selenium.core.workspace.TestWorkspaceProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class RhCheSeleniumSuiteModule extends AbstractModule {
-
-  private Logger LOG = LoggerFactory.getLogger(RhCheSeleniumSuiteModule.class);
-
-  private static final String CHE_MULTIUSER_VARIABLE = "CHE_MULTIUSER";
-  private static final String CHE_INFRASTRUCTURE_VARIABLE = "CHE_INFRASTRUCTURE";
-  @Inject
-  @Named("che.host")
-  private String cheHost;
 
   @Override
   protected void configure() {
@@ -36,11 +23,12 @@ public class RhCheSeleniumSuiteModule extends AbstractModule {
     config.getMap().forEach((key, value) -> bindConstant().annotatedWith(named(key)).to(value));
     bind(DefaultTestUserProvider.class).to(MultiUserCheDefaultTestUserProvider.class);
     bind(DefaultTestUser.class).toProvider(DefaultTestUserProvider.class);
-    install(new FactoryModuleBuilder().build(TestWorkspaceServiceClientFactory.class));
-    bind(TestWorkspaceServiceClient.class).to(AbstractTestWorkspaceServiceClient.class);
-    bind(AbstractTestWorkspaceServiceClient.class).to(RhCheTestWorkspaceServiceClient.class);
+    install(new FactoryModuleBuilder()
+        .implement(TestWorkspaceServiceClient.class, RhCheTestWorkspaceServiceClient.class)
+        .build(TestWorkspaceServiceClientFactory.class)
+    );
+    bind(TestWorkspaceServiceClient.class).to(RhCheTestWorkspaceServiceClient.class);
     bind(TestWorkspaceProvider.class).to(RhCheTestWorkspaceProvider.class).asEagerSingleton();
-    LOG.info("RH-Che Selenium Suite Module successfully loaded for {}", cheHost);
   }
 
 }
