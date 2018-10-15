14b45829517003fc1402a250d37843911b1de414
Tibor Dancs <tdancs@redhat.com>
2018-10-15 11:48:32 +0200

    Fixed dockerfile relative paths

--- dockerfiles/functional-tests/docker-entrypoint.sh
+++ dockerfiles/functional-tests/docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 echo -n Running Xvfb...
 nohup chromedriver &
-/usr/bin/Xvfb :99 -screen 0 1920x1080x24 +extension RANDR
+nohup /usr/bin/Xvfb :99 -screen 0 1920x1080x24 +extension RANDR > /dev/null 2>&1 &
+
+export TARGET="rhel"
+export PR_CHECK_BUILD="true"
+
+pwd
